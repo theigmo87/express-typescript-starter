82a57fe255649923a1dea49d7c58541916d4a6eb
Jimmy Gillam <jgillam@workful.com>
2024-01-30 16:57:23 -0500

    initial check in with typescript hot reload and debugging

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import cors from 'cors';
+import express, {Request, Response} from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`); 
+});
