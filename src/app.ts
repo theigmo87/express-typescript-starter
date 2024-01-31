22110c4b6c48618e8a9d1214418002d85423dd14
Jimmy Gillam <jgillam@workful.com>
2024-01-31 06:31:24 -0500

    adding eslint and prettier configs. need to adjust fix on save settings

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 import cors from 'cors';
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 const port = 3000;
+const test = 0;
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+    res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`); 
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
